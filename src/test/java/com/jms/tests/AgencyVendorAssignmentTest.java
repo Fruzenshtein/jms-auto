6fb8949c02ecbe2f1692e943f8a5f4271f62d007
alex.zvolinskiy <oleksii.zvolinskyi@universalmind.com>
2014-07-14 13:15:18 +0300

    TC 6.2.2

--- src/test/java/com/jms/tests/AgencyVendorAssignmentTest.java
+++ src/test/java/com/jms/tests/AgencyVendorAssignmentTest.java
@@ -54,6 +54,7 @@ public class AgencyVendorAssignmentTest extends BasicTest {
 	
 	@Test
 	@Screenshots(onlyOnFailures = true)
+	//TC6.2.1
 	public void recommendedAgencyVendorApproval() {
 		loginSteps.login(userStorage.getUser(0));
 		globalSteps.searchJobById("2012");
@@ -117,5 +118,52 @@ public class AgencyVendorAssignmentTest extends BasicTest {
 		jobDetailSteps.isBeacon(Beacon.VIDEOGRAPHER_ASSIGNED, BeaconState.GRAY);	
 	}
 
-
+	@Test
+	@Screenshots(onlyOnFailures = true)
+	//TC6.2.2
+	public void recommendedAgencyVendorReassignment() {
+		loginSteps.login(userStorage.getUser(0));
+		globalSteps.searchJobById("2013");
+		jobDetailSteps.clickExpectedServicesButton();
+		globalSteps.waitUntilTextAppear("Special Instructions / Job Info");
+		
+		if (expectedServicesSteps.statusOfServiceIcon(VendorService.REPORTER))
+			expectedServicesSteps.clickVendorServiceIconSection(VendorService.REPORTER);
+		
+		expectedServicesSteps.clickUpdate();
+		
+		globalSteps.pause(6);
+		jobDetailSteps.clickSave();		
+		globalSteps.pause(4);
+		
+		jobDetailSteps.clickExpectedServicesButton();
+		globalSteps.waitUntilTextAppear("Special Instructions / Job Info");
+		expectedServicesSteps.clickVendorServiceIconSection(VendorService.REPORTER);
+		expectedServicesSteps.clickUpdate();
+		globalSteps.pause(14);
+		
+		jobDetailSteps.clickSave();
+		
+		globalSteps.pause(8);
+		
+		jobDetailSteps.clickManageButton();
+		jobDetailSteps.clickVendorActionLink(1);
+		jobDetailSteps.clickVendorsActionMenuLink(1, ActionLink.SELECT_ANOTHER_VENDOR);
+		globalSteps.pause(10);
+		expectedServicesSteps.clickAgencyByIndex(1);
+		expectedServicesSteps.clickChangeRecommendationButton();
+		globalSteps.pause(3);
+		assignVendorSteps.clickSaveButton();
+		globalSteps.waitUntilTextAppear("Recommendation (Agency)");
+		globalSteps.pause(10);
+		
+		//Test starts
+		jobDetailSteps.clickVendorActionLink(1);
+		jobDetailSteps.clickVendorsActionMenuLink(1, ActionLink.RECOMMEND_ANOTHER_VEND);
+		globalSteps.pause(5);
+		
+		String currentVandor = jobDetailSteps.getVendorNameInManageModule(1);
+		
+		globalSteps.waitUntilTextDisappear(currentVandor);
+	}
 }
